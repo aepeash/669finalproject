1f08f6efb6e1554a32c0c1eaaa927b249182fb94
alannahpeash <aepeash@gmail.com>
2020-12-02 08:35:37 -0500

    added hs data

--- HomeScreen.js
+++ HomeScreen.js
@@ -16,9 +16,76 @@ export class HomeScreen extends React.Component {
         this.self = this.props.route.params.currentUser;
 
         this.state = {
-            posts: []
+            posts: [
+                {
+                    key: '1',
+                    author: {
+                        key: 'Yf2XubR8GkQ1VFn9hH33',
+                        displayName: 'Alannah',
+                        email: 'a@a.com',
+                    },
+                    
+                    title: 'Alannah\'s First Post',
+                    description: 'Hey all you cool cats and kittens, this is my first project description.',
+                    timestamp: 'November 25, 2020 at 12:00:00 AM UTC-5'
+                },
+
+                {
+                    key: '2',
+                    author: {
+                        key: '5ZqfNfaCoysNix7E9IV5',
+                        displayName: 'Alannah',
+                        email: 'a@a.com',
+                    },
+
+                    title: 'Alannah\'s second Post',
+                    description: 'This is my description for a project.',
+                    timestamp: 'November 30, 2020 at 12:00:00 AM UTC-5'
+                },
+
+                {
+                    key: '3',
+                    author: {
+                        key: '5ZqfNfaCoysNix7E9IV5',
+                        displayName: 'Alannah',
+                        email: 'a@a.com',
+                    },
+
+                    title: 'Alannah\'s third Post',
+                    description: 'This is my second post description.',
+                    timestamp: 'December 1, 2020 at 12:00:00 AM UTC-5'
+                },
+
+                {
+                    key: '4',
+                    author: {
+                        key: 'Yf2XubR8GkQ1VFn9hH33',
+                        displayName: 'Alannah',
+                        email: 'a@a.com',
+                    },
+
+                    title: 'Alannah\'s fourth Post',
+                    description: 'I was too lazy to fill out a description lmao.',
+                    timestamp: 'December 2, 2020 at 12:00:00 AM UTC-5'
+                },
+
+                {
+                    key: '5',
+                    author: {
+                        key: '5ZqfNfaCoysNix7E9IV5',
+                        displayName: 'Alannah',
+                        email: 'a@a.com',
+                    },
+
+                    title: 'Alannah\'s fifth Post',
+                    description: 'Happy birthday to me :)',
+                    timestamp: 'December 1, 2020 at 12:00:00 AM UTC-5'
+                },
+            ]
         };
     }
+
+
 
     componentDidMount = () => {
       // Executed the first time the component is mounted
